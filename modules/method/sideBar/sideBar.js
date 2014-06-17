b2f2ddae9395513c46b2a7e31311bdd5c30a9fc7
Philpp Bisson <philipp.bisson@gmail.com>
2014-06-17 17:54:12 +0200

    resolve state parameters from app.js

--- modules/method/sideBar/sideBar.js
+++ modules/method/sideBar/sideBar.js
@@ -1,5 +1,7 @@
 angular.module('Methods.io').controller('SidebarCtrl',function($scope,stepService){
+    var protocol  = stepService[0].content.procedure;
+    var steps = protocol[0].processSteps;
 
-    $scope.model = stepService.procedure[0].processSteps;
+    $scope.model = steps
 
 });
