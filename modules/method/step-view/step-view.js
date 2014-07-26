4a2762860237a32672b0563ca9502890c30c1e30
Philpp Bisson <philipp.bisson@gmail.com>
2014-07-26 14:20:07 +0200

    ceanup

--- modules/method/step-view/step-view.js
+++ modules/method/step-view/step-view.js
@@ -1,9 +1,11 @@
 angular.module('Methods.io').controller('StepViewCtrl',function($scope,stepService){
 
+
     var protocol  = stepService[0].content.procedure;
     var steps = protocol[0].processSteps;
 
     $scope.protocol = protocol;
     $scope.steps = steps;
+    $scope.debug = false;
 
 });
